4541239a12d6e9857f632de3e112f0e41e0ca1e1
VWVFQYQ <dennis.hooth@volkswagen.de>
2020-04-03 13:30:53 +0200

    add repositories

--- spring/persistence/src/main/java/de/spring/persistence/repository/DomainRepository.java
+++ spring/persistence/src/main/java/de/spring/persistence/repository/DomainRepository.java
@@ -1,12 +1,16 @@
 package de.spring.persistence.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-/**
- * Domain Repository class
- */
-// @Repository
-// public interface DomainRepository extends JpaRepository<String, Long> {
+import de.spring.persistence.entity.DomainEntity;
 
+// @Repository
+// public interface DomainRepository extends JpaRepository<DomainEntity, Long> {
+
+// 	List<DomainEntity> findAllDomainsByProject(Long projectId);
+
+// 	void setDomainSelected(Long domainId, boolean selected);
 // }
---
+++ spring/persistence/src/main/java/de/spring/persistence/repository/ProjectRepository.java
@@ -0,0 +1,17 @@
+package de.spring.persistence.repository;
+
+import java.io.Serializable;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import de.spring.persistence.entity.ProjectEntity;
+
+// @Repository
+// public interface ProjectRepository extends JpaRepository<ProjectEntity, Long> {
+	
+// 	//public ProjectEntity findProjectByIdOrThrowExeption(Long projectid) throws NotificationException;
+// }
+
+
+    
