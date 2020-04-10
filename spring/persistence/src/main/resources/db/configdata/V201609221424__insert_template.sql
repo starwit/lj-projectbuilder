f3ef1ad676e26c7fcbdc7171101af08b61e47b9c
Anett Hübner <anett_huebner@gmx.de>
2020-04-10 23:28:21 +0200

    not needed sql scipts deleted

--- spring/persistence/src/main/resources/db/configdata/V201609221424__insert_template.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO TEMPLATE (LOCATION,PREFIX,TITLE,BRANCH) VALUES ('https://github.com/witchpou/lirejarp.git','starwit','lirejarp','master');
-INSERT INTO TEMPLATE (LOCATION,PREFIX,TITLE,BRANCH) VALUES ('https://github.com/witchpou/lirejarp.git','starwit','lirejarp','develop');
