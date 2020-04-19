7b9a3bb3fb3cae4342d4bab2117f173edf792250
Anett Hübner <anett_huebner@gmx.de>
2020-04-19 16:42:54 +0200

    add oauth

--- spring/application/src/main/java/de/starwit/application/Application.java
+++ spring/application/src/main/java/de/starwit/application/Application.java
@@ -2,6 +2,11 @@ package de.starwit.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 /**
  * Main SpringApplication to start the whole project
@@ -12,5 +17,42 @@ public class Application {
     public static void main(String[] args) {
         SpringApplication.run(new Class[]{Application.class}, args);
     }
-
+    
+    @Configuration
+    @Profile("dev")
+    static class ApplicationSecurityDev extends WebSecurityConfigurerAdapter {
+ 
+        @Override
+        public void configure(WebSecurity web) throws Exception {
+            web
+                    .ignoring()
+                    .antMatchers("/**");
+        }
+    }
+    
+    @Configuration
+    @Profile("!dev")
+    static class ApplicationSecurityProd extends WebSecurityConfigurerAdapter {
+ 
+        @Override
+        public void configure(WebSecurity web) throws Exception {
+        	web
+            .ignoring()
+            .antMatchers("/welcome.html");
+        	
+        }
+        
+    	@Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http
+            	.authorizeRequests()
+                    .antMatchers("/welcome.html").permitAll()
+                    .antMatchers("/**").access("hasRole('ROLE_USER')")
+                    .anyRequest().authenticated()
+                    .and()
+                .oauth2Login()
+                    .permitAll();
+        }
+    }
+    
 }
