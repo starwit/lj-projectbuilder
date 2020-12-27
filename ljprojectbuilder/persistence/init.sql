305d68f61e29730ff7d02fb61a3b39e1fea25091
Anett Hübner <anett_huebner@gmx.de>
2020-12-27 20:55:30 +0100

    restructure project

--- ljprojectbuilder/persistence/init.sql
+++
@@ -1,3 +0,0 @@
-CREATE DATABASE IF NOT EXISTS ljprojectbuider;
-CREATE USER 'ljprojectbuilder'@'localhost' IDENTIFIED BY 'ljprojectbuider';
-GRANT ALL PRIVILEGES ON base.* To 'ljprojectbuilder'@'localhost';
