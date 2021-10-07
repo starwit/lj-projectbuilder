6358a226a1e71cf9c9be3ea11a4465369c984f76
Cedric Rische <cedricrische@cedsoft.de>
2021-10-07 20:29:19 +0200

    Outsourced Accordion of Field edit in FieldAccordion.jsx

---
+++ webclient/app/src/app/commons/fieldAccordion/FieldAccordion.jsx
@@ -0,0 +1,135 @@
+import React from "react";
+import {
+    Accordion,
+    AccordionDetails,
+    AccordionSummary, Checkbox, Divider,
+    FormControl, FormControlLabel,
+    Grid,
+    InputLabel, MenuItem, Select,
+    TextField,
+    Typography
+} from "@mui/material";
+import {ExpandMore} from "@mui/icons-material";
+import PropTypes from "prop-types";
+
+
+function FieldAccordion(props) {
+
+    const {dataType, mandatory, min, max, pattern, description, name, editFieldProperty, dataTypes} = props;
+
+    function renderAccordionTitle(name) {
+        let value = "Neues Feld";
+        if (name) {
+            value = name;
+        }
+        return value;
+    }
+
+    return (
+        <Accordion>
+            <AccordionSummary
+                expandIcon={<ExpandMore/>}
+                aria-controls="panel1a-content"
+                id="panel1a-header"
+            >
+                <Typography sx={{flexShrink: 0, width: "50%"}}>{renderAccordionTitle(name)}</Typography>
+                <Typography
+                    sx={{color: 'text.secondary'}}>{/* Add something interesting here */}</Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+                <Grid container spacing={4}>
+                    <Grid item sm={6}>
+                        <TextField fullWidth value={name} label={"Name"}
+                                   onChange={(event) => editFieldProperty("name", event.target.value)}/>
+                    </Grid>
+                    <Grid item sm={6}>
+                        <FormControl fullWidth>
+                            <InputLabel id="demo-simple-select-label">Datentyp</InputLabel>
+                            <Select
+                                labelId="demo-simple-select-label"
+                                id="demo-simple-select"
+                                value={dataType.id}
+                                label="Datatype"
+                                onChange={(event) => editFieldProperty("dataType", event.target.value)}
+                            >
+                                {dataTypes.map(dataType => (
+                                    <MenuItem value={dataType.id}>{dataType.name}</MenuItem>
+                                ))}
+                            </Select>
+                        </FormControl>
+                    </Grid>
+                    <Grid item sm={12}>
+                        <TextField
+                            fullWidth
+                            label={"Beschreibung"}
+                            value={description}
+                            onChange={(event) => editFieldProperty("description", event.target.value)}
+                        />
+                    </Grid>
+                    <Grid item sm={12}>
+                        <Divider variant={"middle"}>Restriktionen</Divider>
+                    </Grid>
+                    <Grid item sm={12}>
+                        <TextField
+                            fullWidth
+                            label={"Pattern"}
+                            value={pattern}
+                            onChange={(event) => editFieldProperty("pattern", event.target.value)}
+                        />
+                    </Grid>
+                    <Grid item sm={6}>
+                        <TextField
+                            fullWidth
+                            label={"Min"}
+                            value={min}
+                            disabled={!dataType.allowMin}
+                            onChange={(event) => editFieldProperty("min", event.target.value)}
+                        />
+                    </Grid>
+                    <Grid item sm={6}>
+                        <TextField
+                            fullWidth
+                            label={"Max"}
+                            value={max}
+                            disabled={!dataType.allowMax}
+                            onChange={(event) => editFieldProperty("max", event.target.value)}
+                        />
+                    </Grid>
+                    <Grid item sm={12}>
+                        <FormControlLabel
+                            control={<Checkbox defaultChecked/>}
+                            label="Pflichtfeld"
+                            value={mandatory}
+                            onChange={(event) => editFieldProperty("mandatory", !mandatory)}
+                        />
+                    </Grid>
+                </Grid>
+            </AccordionDetails>
+        </Accordion>
+    )
+}
+
+FieldAccordion.propTypes = {
+    dataType: PropTypes.object,
+    mandatory: PropTypes.bool,
+    min: PropTypes.any,
+    max: PropTypes.any,
+    pattern: PropTypes.string,
+    description: PropTypes.string,
+    name: PropTypes.string,
+    editFieldProperty: PropTypes.func.isRequired,
+    dataTypes: PropTypes.array,
+};
+
+FieldAccordion.defaultProps = {
+    dataType: "",
+    mandatory: false,
+    min: "",
+    max: "",
+    pattern: "",
+    description: "",
+    name: "",
+    dataTypes: []
+}
+
+export default FieldAccordion;
--- webclient/app/src/app/pages/projectEditor/sections/erDesigner/entityEditor/EntityEditor.jsx
+++ webclient/app/src/app/pages/projectEditor/sections/erDesigner/entityEditor/EntityEditor.jsx
@@ -13,6 +13,7 @@ import {
 } from "@mui/material";
 import {Add, Close, ExpandMore} from "@mui/icons-material";
 import LoadingSpinner from "../../../../../commons/loadingSpinner/LoadingSpinner";
+import FieldAccordion from "../../../../../commons/fieldAccordion/FieldAccordion";
 
 
 function TabPanel(props) {
@@ -154,111 +155,35 @@ function EntityEditor(props) {
         setEntity(newEntity);
     }
 
-    function handleFieldDataTypeChange(dataTypeId, index) {
-        const newEntity = {...entity};
-        const foundDataTypeObject = dataTypes.find(dataType => dataType.id === dataTypeId);
-        newEntity.fields[index].dataType = foundDataTypeObject;
-        setEntity(newEntity)
-    }
-
     function editFieldProperty(key, value, index) {
         const newEntity = {...entity};
+        if (key === "dataType") {
+            value = dataTypes.find(dataType => dataType.id === value);
+        }
         newEntity.fields[index][key] = value;
         setEntity(newEntity)
     }
 
-    function renderAccordionTitle(name) {
-        let value = "Neues Feld";
-        if (name) {
-            value = name;
-        }
-        return value;
+
+    function renderRelations() {
+
     }
 
     function renderFields() {
         return entity.fields.map((field, index) => {
             const {dataType, mandatory, min, max, pattern, description, name} = entity.fields[index];
             return (
-                <Accordion>
-                    <AccordionSummary
-                        expandIcon={<ExpandMore/>}
-                        aria-controls="panel1a-content"
-                        id="panel1a-header"
-                    >
-                        <Typography sx={{flexShrink: 0, width: "50%"}}>{renderAccordionTitle(field.name)}</Typography>
-                        <Typography
-                            sx={{color: 'text.secondary'}}>{/* Add something interesting here */}</Typography>
-                    </AccordionSummary>
-                    <AccordionDetails>
-                        <Grid container spacing={4}>
-                            <Grid item sm={6}>
-                                <TextField fullWidth value={name} label={"Name"}
-                                           onChange={(event) => editFieldProperty("name", event.target.value, index)}/>
-                            </Grid>
-                            <Grid item sm={6}>
-                                <FormControl fullWidth>
-                                    <InputLabel id="demo-simple-select-label">Datentyp</InputLabel>
-                                    <Select
-                                        labelId="demo-simple-select-label"
-                                        id="demo-simple-select"
-                                        value={dataType.id}
-                                        label="Datatype"
-                                        onChange={(event) => handleFieldDataTypeChange(event.target.value, index)}
-                                    >
-                                        {dataTypes.map(dataType => (
-                                            <MenuItem value={dataType.id}>{dataType.name}</MenuItem>
-                                        ))}
-                                    </Select>
-                                </FormControl>
-                            </Grid>
-                            <Grid item sm={12}>
-                                <TextField
-                                    fullWidth
-                                    label={"Beschreibung"}
-                                    value={description}
-                                    onChange={(event) => editFieldProperty("description", event.target.value, index)}
-                                />
-                            </Grid>
-                            <Grid item sm={12}>
-                                <Divider variant={"middle"}>Restriktionen</Divider>
-                            </Grid>
-                            <Grid item sm={12}>
-                                <TextField
-                                    fullWidth
-                                    label={"Pattern"}
-                                    value={pattern}
-                                    onChange={(event) => editFieldProperty("pattern", event.target.value, index)}
-                                />
-                            </Grid>
-                            <Grid item sm={6}>
-                                <TextField
-                                    fullWidth
-                                    label={"Min"}
-                                    value={min}
-                                    disabled={!dataType.allowMin}
-                                    onChange={(event) => editFieldProperty("min", event.target.value, index)}
-                                />
-                            </Grid>
-                            <Grid item sm={6}>
-                                <TextField
-                                    fullWidth
-                                    label={"Max"}
-                                    value={max}
-                                    disabled={!dataType.allowMax}
-                                    onChange={(event) => editFieldProperty("max", event.target.value, index)}
-                                />
-                            </Grid>
-                            <Grid item sm={12}>
-                                <FormControlLabel
-                                    control={<Checkbox defaultChecked/>}
-                                    label="Pflichtfeld"
-                                    value={mandatory}
-                                    onChange={(event) => editFieldProperty("mandatory", !mandatory, index)}
-                                />
-                            </Grid>
-                        </Grid>
-                    </AccordionDetails>
-                </Accordion>
+                <FieldAccordion
+                    editFieldProperty={(key, value) => editFieldProperty(key, value, index)}
+                    dataTypes={dataTypes}
+                    dataType={dataType}
+                    description={description}
+                    pattern={pattern}
+                    min={min}
+                    max={max}
+                    mandatory={mandatory}
+                    name={name}
+                />
             )
         })
 
@@ -299,7 +224,8 @@ function EntityEditor(props) {
                         <Button fullWidth startIcon={<Add/>} onClick={addField}>Hinzufügen</Button>
                     </TabPanel>
                     <TabPanel value={value} index={1}>
-                        Relationen
+                        {renderRelations()}
+                        <Button fullWidth startIcon={<Add/>} onClick={addField}>Hinzufügen</Button>
                     </TabPanel>
                 </Box>
                 <DialogActions>
