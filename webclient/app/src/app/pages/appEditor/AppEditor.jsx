3b9ace29365e84eedee0f493af35a375c5022057
Cedric Rische <cedricrische@cedsoft.de>
2021-12-19 18:37:23 +0100

    feature/#140 Remove useEffect

--- webclient/app/src/app/pages/appEditor/AppEditor.jsx
+++ webclient/app/src/app/pages/appEditor/AppEditor.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from "react";
+import React, {useState} from "react";
 import {Box, Button, Step, StepLabel, Stepper} from "@mui/material";
 import TemplateSelection from "./sections/templateSection/TemplateSection";
 import ErDesigner from "./sections/erSection/ErSection";
