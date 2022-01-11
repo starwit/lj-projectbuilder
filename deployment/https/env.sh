b1f137a08dc2871e50780f7be02aea70fc083bae
Anett Hübner <anett_huebner@gmx.de>
2022-01-11 14:29:30 +0100

    feature/ab#46:fixed prod deployment

--- deployment/https/env.sh
+++ deployment/https/env.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-export DB_PW_PROJECTBUILDER=tobedefined
-export KEYCLOAK_PW=tobedefined
-export DB_PW_KEYCLOAK=tobedefined
-export DB_PW_PB_ROOT=tobedefined
-export DB_PW_KC_ROOT=tobedefined
-export DB_PW_PB_ROOT=tobedefined
+export DB_PW_PROJECTBUILDER=
+export KEYCLOAK_PW=
+export DB_PW_KEYCLOAK=
+export DB_PW_PB_ROOT=
+export DB_PW_KC_ROOT=
+export DB_PW_PB_ROOT=
+export KEYCLOAK_USER="projectbuilderadmin"
+export DOMAIN="pb.starwit.de"
