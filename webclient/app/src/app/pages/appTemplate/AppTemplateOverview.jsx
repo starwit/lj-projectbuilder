d6997d6dca1cf599cbbd3afad414954341c00c90
Anett Hübner <anett_huebner@gmx.de>
2022-01-20 14:08:19 +0100

    feature/ab#97: add-button for apptemplate changed

--- webclient/app/src/app/pages/appTemplate/AppTemplateOverview.jsx
+++ webclient/app/src/app/pages/appTemplate/AppTemplateOverview.jsx
@@ -1,15 +1,18 @@
-import { Container, Grid, Typography, Button } from "@mui/material";
+import { Container, Grid, Typography, Fab } from "@mui/material";
 import React, { useState, useEffect } from "react";
 import { useTranslation } from "react-i18next";
 import AppTemplateCard from "../../commons/appTemplateCard/AppTemplateCard";
 import AppTemplateDialog from "../../commons/appTemplateDialog/AppTemplateDialog";
 import AppTemplateRest from "../../services/AppTemplateRest";
+import AppTemplateOverviewStyles from "./AppTempateOverviewStyles";
+import { Add } from "@mui/icons-material";
 
 function AppTemplateOverview() {
     const { t } = useTranslation();
     const [openDialog, setOpenDialog] = React.useState(false);
     const [selectedAppTemplate, setSelectedAppTemplate] = useState(false);
-    const [data, setData] = useState([]);    
+    const [data, setData] = useState([]);
+    const appTemplateOverviewStyles = AppTemplateOverviewStyles();    
 
     const handleDialogOpen = () => {
         setOpenDialog(true);
@@ -47,10 +50,6 @@ function AppTemplateOverview() {
                         {t("appTemplateOverview.title")}
                     </Typography>
                 </Grid>
-                <Grid item xs={3} align="right" >
-                    <br />
-                    <Button variant="contained" onClick={handleDialogOpen} >{t("button.create")}</Button>
-                </Grid>
             </Grid>
                 <Grid container spacing={2}>
                     {data.map(appTemplate => (
@@ -66,6 +65,11 @@ function AppTemplateOverview() {
                 onRefresh={reload}
                 isCreateDialog={true}
             />
+            <div className={appTemplateOverviewStyles.addFab}>
+                <Fab color="primary" aria-label="add" onClick={handleDialogOpen}>
+                    <Add/>
+                </Fab>
+            </div>
         </Container>
     )
 }
