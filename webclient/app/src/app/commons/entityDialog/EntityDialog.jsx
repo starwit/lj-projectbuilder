b08f2978ccc3041c6814de3d717b13c18b6cbc65
Cedric Rische <cedric.rische@cedsoft.de>
2022-01-26 23:39:22 +0100

    bugfix/AB#110 Added Entity title to EntityDialog Title

--- webclient/app/src/app/commons/entityDialog/EntityDialog.jsx
+++ webclient/app/src/app/commons/entityDialog/EntityDialog.jsx
@@ -260,7 +260,13 @@ function EntityDialog(props) {
     return (
         <Dialog open={!!entityId} maxWidth={"xl"} fullWidth>
             <DialogTitle className={entityEditorStyles.dialogHeaderBar}>
-                <Typography noWrap variant={"h6"} component={"p"}>{t("entityDialog.editDomain")}</Typography>
+                <Typography
+                    noWrap
+                    variant={"h6"}
+                    component={"p"}
+                >
+                    {t("entityDialog.editDomain", {entityName: entity.name})}
+                </Typography>
                 <div className={entityEditorStyles.flex}/>
                 <IconButton
                     aria-label="close"
