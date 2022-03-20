ae79e092a758dcbffae476da574e2282d4f7cbc2
Cedric Rische <cedric.rische@cedsoft.de>
2022-03-20 18:25:08 +0100

    feature/AB#77

--- webclient/app/src/app/features/appTemplates/appTemplateCard/AppTemplateCard.jsx
+++ webclient/app/src/app/features/appTemplates/appTemplateCard/AppTemplateCard.jsx
@@ -1,28 +1,14 @@
-import React, {useEffect, useState} from "react";
-import {styled} from '@mui/material/styles';
-import {
-    Card,
-    CardContent,
-    Chip,
-    Collapse,
-    Container,
-    Divider,
-    Grid,
-    IconButton,
-    List,
-    ListItem,
-    ListItemIcon,
-    ListItemText,
-    Typography
-} from "@mui/material";
+import React, { useEffect, useState } from "react";
+import { styled } from "@mui/material/styles";
+import { Card, CardContent, Chip, Collapse, Container, Divider, Grid, IconButton, List, ListItem, ListItemIcon, ListItemText, Typography } from "@mui/material";
 import AppTemplateCardStyles from "./AppTemplateCardStyles";
-import {useTranslation} from "react-i18next";
-import GitHub from '@mui/icons-material/GitHub';
-import {Delete, Edit} from "@mui/icons-material";
+import { useTranslation } from "react-i18next";
+import GitHub from "@mui/icons-material/GitHub";
+import { Delete, Edit } from "@mui/icons-material";
 import SyntaxHighlighter from "react-syntax-highlighter/dist/esm/light";
-import js from 'react-syntax-highlighter/dist/esm/languages/hljs/javascript';
-import docco from 'react-syntax-highlighter/dist/esm/styles/hljs/docco';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import js from "react-syntax-highlighter/dist/esm/languages/hljs/javascript";
+import docco from "react-syntax-highlighter/dist/esm/styles/hljs/docco";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import AppTemplateDialog from "./../appTemplateDialog/AppTemplateDialog";
 import AppTemplateRest from "../../../services/AppTemplateRest";
 import GitRest from "../../../services/GitRest";
@@ -31,23 +17,21 @@ import NotificationDialog from "../../../commons/alert/NotificationDialog";
 import GitDataButton from "../../../commons/gitDownloadButton/GitDataButton";
 
 const ExpandMore = styled((props) => {
-    const {expand, ...other} = props;
+    const { expand, ...other } = props;
     return <IconButton {...other} />;
-})(({theme, expand}) => ({
-    transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
-    marginLeft: 'auto',
-    transition: theme.transitions.create('transform', {
+})(({ theme, expand }) => ({
+    transform: !expand ? "rotate(0deg)" : "rotate(180deg)",
+    marginLeft: "auto",
+    transition: theme.transitions.create("transform", {
         duration: theme.transitions.duration.shortest,
     }),
 }));
 
-
 function AppTemplateCard(props) {
-
     const appTemplateCardStyles = AppTemplateCardStyles();
-    const {t} = useTranslation();
+    const { t } = useTranslation();
 
-    const {appTemplate, handleRefresh, userGroups} = props;
+    const { appTemplate, handleRefresh, userGroups } = props;
     const appTemplateRest = new AppTemplateRest();
     const gitRest = new GitRest();
     const [selectedAppTemplate, setSelectedAppTemplate] = useState(false);
@@ -58,16 +42,16 @@ function AppTemplateCard(props) {
 
     const [openDeleteDialog, setOpenDeleteDialog] = useState(false);
 
-    const deleteErrorDialogContent = ({
-        "title": t("appTemplate.error.title"),
-        "message": t("appTemplate.error.message")
-    });
+    const deleteErrorDialogContent = {
+        title: t("appTemplate.error.title"),
+        message: t("appTemplate.error.message"),
+    };
     const [openErrorDialog, setOpenErrorDialog] = useState(false);
 
-    const successDialogContent = ({
-        "title": t("appTemplate.success.title"),
-        "message": t("appTemplate.success.message")
-    });
+    const successDialogContent = {
+        title: t("appTemplate.success.title"),
+        message: t("appTemplate.success.message"),
+    };
     const [openSuccessDialog, setOpenSuccessDialog] = useState(false);
 
     const closeDeleteDialog = () => {
@@ -89,19 +73,21 @@ function AppTemplateCard(props) {
     };
 
     const handleDelete = (appTemplateId) => {
-        appTemplateRest.delete(appTemplateId)
+        appTemplateRest
+            .delete(appTemplateId)
             .then(() => {
                 handleRefresh();
-            }).catch(err => {
-            setOpenErrorDialog(true);
-            setOpenDeleteDialog(false);
-            console.log(err.response.data);
-        });
+            })
+            .catch((err) => {
+                setOpenErrorDialog(true);
+                setOpenDeleteDialog(false);
+                console.log(err.response.data);
+            });
     };
 
     const handleGit = (downloadRequestData) => {
         return gitRest.updateTemplates(appTemplate.id, downloadRequestData);
-    }
+    };
 
     const handleAfterGitSuccess = () => {
         handleRefresh();
@@ -110,13 +96,13 @@ function AppTemplateCard(props) {
 
     const loadAppTemplate = (appTemplateId) => {
         const appTemplateRest2 = new AppTemplateRest();
-        appTemplateRest2.findById(appTemplateId).then(response => {
+        appTemplateRest2.findById(appTemplateId).then((response) => {
             setExtendedAppTemplate(response.data);
         });
     };
 
     useEffect(() => {
-        SyntaxHighlighter.registerLanguage('javascript', js);
+        SyntaxHighlighter.registerLanguage("javascript", js);
     }, []);
 
     return (
@@ -130,48 +116,49 @@ function AppTemplateCard(props) {
                             </Typography>
                         </Grid>
                         <Grid item xs={5} align="right">
-                            <IconButton onClick={handleDialogOpen}><Edit fontSize={"small"}/></IconButton>
-                            <IconButton onClick={() => setOpenDeleteDialog(true)}><Delete
-                                fontSize={"small"}/></IconButton>
+                            <IconButton onClick={handleDialogOpen}>
+                                <Edit fontSize={"small"} />
+                            </IconButton>
+                            <IconButton onClick={() => setOpenDeleteDialog(true)}>
+                                <Delete fontSize={"small"} />
+                            </IconButton>
                         </Grid>
                     </Grid>
                 </CardContent>
-                <Divider/>
+                <Divider />
                 <CardContent>
                     <Grid container spacing={0}>
                         <Grid item sm={7}>
                             <Typography variant="body2" color="text.secondary">
-                                <br/>{appTemplate.description}
+                                <br />
+                                {appTemplate.description}
                             </Typography>
                             <List>
                                 <ListItem disablePadding>
                                     <ListItemIcon>
-                                        <GitHub/>
+                                        <GitHub />
                                     </ListItemIcon>
-                                    <ListItemText primary={appTemplate.location}
-                                                  secondary={t("appTemplate.branch") + ": " + appTemplate.branch}/>
+                                    <ListItemText primary={appTemplate.location} secondary={t("appTemplate.branch") + ": " + appTemplate.branch} />
                                 </ListItem>
                             </List>
                         </Grid>
                         <Grid item xs={5} align="right">
-                            <br/><br/>
+                            <br />
+                            <br />
                             <GitDataButton
                                 credentialsRequired={appTemplate.credentialsRequired}
                                 handleAfterSuccess={handleAfterGitSuccess}
                                 handleGit={handleGit}
-                                buttonName={t("button.loadtemplate")}/>
+                                buttonName={t("button.loadtemplate")}
+                            />
                         </Grid>
                     </Grid>
                 </CardContent>
 
                 <Divider>
-                    <Chip label={t("appTemplate.config")}/>
-                    <ExpandMore
-                        expand={expanded}
-                        onClick={() => handleExpandClick(appTemplate.id)}
-                        aria-expanded={expanded}
-                        aria-label="show more">
-                        <ExpandMoreIcon/>
+                    <Chip label={t("appTemplate.config")} />
+                    <ExpandMore expand={expanded} onClick={() => handleExpandClick(appTemplate.id)} aria-expanded={expanded} aria-label="show more">
+                        <ExpandMoreIcon />
                     </ExpandMore>
                 </Divider>
                 <Collapse in={expanded} timeout="auto" unmountOnExit>
@@ -181,9 +168,11 @@ function AppTemplateCard(props) {
                             style={docco}
                             showLineNumbers
                             customStyle={{
-                                lineHeight: "1.5", fontSize: ".75em"
-                            }}>
-                            {JSON.stringify(extendedAppTemplate, null, '\t')}
+                                lineHeight: "1.5",
+                                fontSize: ".75em",
+                            }}
+                        >
+                            {JSON.stringify(extendedAppTemplate, null, "\t")}
                         </SyntaxHighlighter>
                     </CardContent>
                 </Collapse>
@@ -201,27 +190,14 @@ function AppTemplateCard(props) {
                 message={t("appTemplate.delete.message")}
                 open={openDeleteDialog}
                 onClose={closeDeleteDialog}
-                onSubmit={
-                    () => handleDelete(appTemplate.id)
-                }
+                onSubmit={() => handleDelete(appTemplate.id)}
                 confirmTitle={t("button.delete")}
+            />
+            <NotificationDialog severity="error" content={deleteErrorDialogContent} open={openErrorDialog} onClose={() => setOpenErrorDialog(false)} />
 
-            />
-            <NotificationDialog
-                severity="error"
-                content={deleteErrorDialogContent}
-                open={openErrorDialog}
-                onClose={() => setOpenErrorDialog(false)}
-            />
-
-            <NotificationDialog
-                severity="success"
-                content={successDialogContent}
-                open={openSuccessDialog}
-                onClose={() => setOpenSuccessDialog(false)}
-            />
+            <NotificationDialog severity="success" content={successDialogContent} open={openSuccessDialog} onClose={() => setOpenSuccessDialog(false)} />
         </Container>
-    )
+    );
 }
 
 export default AppTemplateCard;
