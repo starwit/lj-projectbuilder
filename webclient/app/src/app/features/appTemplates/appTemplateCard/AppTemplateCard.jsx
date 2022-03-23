067f6ba7307304dc2a52b725e552ffa13477a46e
DerCed <DerCed@users.noreply.github.com>
2022-03-23 22:22:57 +0100

    bugfix/AB#189 (#271)
    added return
    
    Co-authored-by: Cedric Rische <cedric.rische@cedsoft.de>

--- webclient/app/src/app/features/appTemplates/appTemplateCard/AppTemplateCard.jsx
+++ webclient/app/src/app/features/appTemplates/appTemplateCard/AppTemplateCard.jsx
@@ -62,7 +62,7 @@ function AppTemplateCard(props) {
     };
 
     const handleDelete = (appTemplateId) => {
-        appTemplateRest.delete(appTemplateId).then(() => {
+        return appTemplateRest.delete(appTemplateId).then(() => {
             handleRefresh();
         });
     };
