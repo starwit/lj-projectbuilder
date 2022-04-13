eea154bc96f40605046db8406d92d8735cc16b8b
Anett Hübner <anett_huebner@gmx.de>
2022-04-13 17:44:50 +0200

    Update webclient/app/src/app/features/apps/entities/entityDialog/EntityDialog.jsx

--- webclient/app/src/app/features/apps/entities/entityDialog/EntityDialog.jsx
+++ webclient/app/src/app/features/apps/entities/entityDialog/EntityDialog.jsx
@@ -38,7 +38,7 @@ import {useImmer} from "use-immer";
 function EntityDialog(props) {
     const {entityId, onClose, handleSave, entities, open} = props;
     const [value, setValue] = useState(0);
-    const [entity, setEntity] = useImmer({});
+    const [entity, setEntity] = useImmer(null);
     const [hasFormError, setHasFormError] = useState(false);
     const [isSaving, setIsSaving] = useState(false);
     const entityEditorStyles = EntityDialogStyles();
