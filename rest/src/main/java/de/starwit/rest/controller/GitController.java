9f24cefa9157ffe9184c21edacce5fdedb547ba3
Anett Hübner <anett_huebner@gmx.de>
2022-04-14 16:13:48 +0200

    formatting

--- rest/src/main/java/de/starwit/rest/controller/GitController.java
+++ rest/src/main/java/de/starwit/rest/controller/GitController.java
@@ -37,91 +37,93 @@ import io.swagger.v3.oas.annotations.Operation;
 @RequestMapping("${rest.base-path}/git")
 public class GitController {
 
-  final static Logger LOG = LoggerFactory.getLogger(GitController.class);
+    final static Logger LOG = LoggerFactory.getLogger(GitController.class);
 
-  @Autowired
-  private AppSetupService appSetupService;
+    @Autowired
+    private AppSetupService appSetupService;
 
-  @Autowired
-  private AppService appService;
+    @Autowired
+    private AppService appService;
 
-  @Operation(summary = "Creates new App from templates.")
-  @PostMapping(value = "/setup-app/{appId}")
-  public void generateApp(@PathVariable("appId") Long appId, @Valid @RequestBody GitAuthDto dto) throws NotificationException {
-    appSetupService.setupAndGenerateApp(appId, dto);
-  }
+    @Operation(summary = "Creates new App from templates.")
+    @PostMapping(value = "/setup-app/{appId}")
+    public void generateApp(@PathVariable("appId") Long appId, @Valid @RequestBody GitAuthDto dto)
+            throws NotificationException {
+        appSetupService.setupAndGenerateApp(appId, dto);
+    }
 
-  @Operation(summary = "Gets template description from git repository and updates template definitions in database.")
-  @PostMapping(value = "/update-templates/{appTemplateId}")
-  public void updateCodeTemplates(@PathVariable("appTemplateId") Long appTemplateId, @Valid @RequestBody GitAuthDto dto) throws NotificationException {
-    appSetupService.updateTemplates(appTemplateId, dto);
-  }
+    @Operation(summary = "Gets template description from git repository and updates template definitions in database.")
+    @PostMapping(value = "/update-templates/{appTemplateId}")
+    public void updateCodeTemplates(@PathVariable("appTemplateId") Long appTemplateId,
+            @Valid @RequestBody GitAuthDto dto) throws NotificationException {
+        appSetupService.updateTemplates(appTemplateId, dto);
+    }
 
-  @GetMapping("/download-app/{appId}")
-  public void downloadApp(@PathVariable("appId") Long appId, HttpServletResponse response) throws IOException {
+    @GetMapping("/download-app/{appId}")
+    public void downloadApp(@PathVariable("appId") Long appId, HttpServletResponse response) throws IOException {
 
-    response.setContentType("application/octet-stream");
-    response.setHeader("Content-Disposition", "attachment;filename=download.zip");
-    response.setStatus(HttpServletResponse.SC_OK);
+        response.setContentType("application/octet-stream");
+        response.setHeader("Content-Disposition", "attachment;filename=download.zip");
+        response.setStatus(HttpServletResponse.SC_OK);
 
-    App entity = appService.findById(appId);
-    if (entity != null && entity.getTargetPath() != null) {
-      response.setHeader("Content-Disposition", "attachment;filename=" + entity.getTitle() + ".zip");
-      File directory = new File(Constants.TMP_DIR + Constants.FILE_SEP + entity.getTargetPath());
-      File[] files = directory.listFiles();
-      if (files != null && files.length > 0) {
-        try (ZipOutputStream zippedOut = new ZipOutputStream(response.getOutputStream())) {
-          for (File file : files) {
-            if (!Constants.TEMPLATE_DIR.equals(file.getName())) {
-              addDirToZipArchive(zippedOut, file, null);
+        App entity = appService.findById(appId);
+        if (entity != null && entity.getTargetPath() != null) {
+            response.setHeader("Content-Disposition", "attachment;filename=" + entity.getTitle() + ".zip");
+            File directory = new File(Constants.TMP_DIR + Constants.FILE_SEP + entity.getTargetPath());
+            File[] files = directory.listFiles();
+            if (files != null && files.length > 0) {
+                try (ZipOutputStream zippedOut = new ZipOutputStream(response.getOutputStream())) {
+                    for (File file : files) {
+                        if (!Constants.TEMPLATE_DIR.equals(file.getName())) {
+                            addDirToZipArchive(zippedOut, file, null);
+                        }
+                    }
+                    zippedOut.finish();
+                } catch (Exception e) {
+                    LOG.error("Error Downloading App", e);
+                    LOG.error("Error downloading app as ZIP-file", e.getCause());
+                    ServletOutputStream sos = response.getOutputStream();
+                    response.setContentType("application/json");
+                    response.setStatus(HttpServletResponse.SC_NOT_ACCEPTABLE);
+                    sos.print(
+                            "{response : {data : { metadata : {message : \"error.downloadapp\", responseCode : \"ERROR\"} } }");
+                    sos.flush();
+                    sos.close();
+                }
             }
-          }
-          zippedOut.finish();
-        } catch (Exception e) {
-          LOG.error("Error Downloading App", e);
-          LOG.error("Error downloading app as ZIP-file", e.getCause());
-          ServletOutputStream sos = response.getOutputStream();
-          response.setContentType("application/json");
-          response.setStatus(HttpServletResponse.SC_NOT_ACCEPTABLE);
-          sos.print("{response : {data : { metadata : {message : \"error.downloadapp\", responseCode : \"ERROR\"} } }");
-          sos.flush();
-          sos.close();
         }
-      }
-    }
-  }
-
-  public static void addDirToZipArchive(ZipOutputStream zos, File fileToZip, String parrentDirectoryName)
-      throws Exception {
-    if (fileToZip == null || !fileToZip.exists()) {
-      return;
     }
 
-    String zipEntryName = fileToZip.getName();
-    if (".git".equals(zipEntryName)) {
-      return;
-    }
+    public static void addDirToZipArchive(ZipOutputStream zos, File fileToZip, String parrentDirectoryName)
+            throws Exception {
+        if (fileToZip == null || !fileToZip.exists()) {
+            return;
+        }
 
-    if (parrentDirectoryName != null && !parrentDirectoryName.isEmpty()) {
-      zipEntryName = parrentDirectoryName + "/" + fileToZip.getName();
-    }
+        String zipEntryName = fileToZip.getName();
+        if (".git".equals(zipEntryName)) {
+            return;
+        }
 
-    if (fileToZip.isDirectory()) {
-      LOG.info("+{}", zipEntryName);
-      for (File file : fileToZip.listFiles()) {
-        addDirToZipArchive(zos, file, zipEntryName);
-      }
-    } else {
-      LOG.info("   {}", zipEntryName);
-      FileSystemResource resource = new FileSystemResource(fileToZip);
-      ZipEntry zipEntry = new ZipEntry(resource.getFilename());
-      // Configure the zip entry, the properties of the file
-      zipEntry.setSize(resource.contentLength());
-      zipEntry.setTime(System.currentTimeMillis());
-      zos.putNextEntry(new ZipEntry(zipEntryName));
-      StreamUtils.copy(resource.getInputStream(), zos);
-      zos.closeEntry();
-    }
-  }
+        if (parrentDirectoryName != null && !parrentDirectoryName.isEmpty()) {
+            zipEntryName = parrentDirectoryName + "/" + fileToZip.getName();
+        }
 
+        if (fileToZip.isDirectory()) {
+            LOG.debug("+{}", zipEntryName);
+            for (File file : fileToZip.listFiles()) {
+                addDirToZipArchive(zos, file, zipEntryName);
+            }
+        } else {
+            LOG.debug("   {}", zipEntryName);
+            FileSystemResource resource = new FileSystemResource(fileToZip);
+            ZipEntry zipEntry = new ZipEntry(resource.getFilename());
+            // Configure the zip entry, the properties of the file
+            zipEntry.setSize(resource.contentLength());
+            zipEntry.setTime(System.currentTimeMillis());
+            zos.putNextEntry(new ZipEntry(zipEntryName));
+            StreamUtils.copy(resource.getInputStream(), zos);
+            zos.closeEntry();
+        }
+    }
 }
