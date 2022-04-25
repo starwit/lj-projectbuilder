c200b27cc97ed098798e79845e7a240dc5a616f1
Anett Hübner <anett_huebner@gmx.de>
2022-04-25 15:18:11 +0200

    corrected import

--- generator/src/main/java/de/starwit/generator/generator/EntityImports.java
+++ generator/src/main/java/de/starwit/generator/generator/EntityImports.java
@@ -8,56 +8,57 @@ import de.starwit.persistence.entity.DataType;
 import de.starwit.persistence.entity.Domain;
 
 /**
- * 
+ *
  * @author anett
  *
  */
 public class EntityImports {
-	/**
-	 * Imports for Java-Entities Bean Validation and DataTypes for EntityGeneration. 
-	 * @param domain - definition of domain.
-	 * @return Set of Java imports
-	 */
-	public static Set<String> gatherEntityImports(Domain domain) {
-		Set<String> imports = new HashSet<String>();
-		if (domain.getAttributes() != null) {
-			for (Attribute attr : domain.getAttributes()) {
-				
-				if (DataType.String.equals(attr.getDataType())) {
-					if (attr.getMax() != null || attr.getMin() != null) {
-						imports.add("import javax.validation.constraints.Size;");
-					}
-					if (!attr.isNullable()) {
-						imports.add("import org.hibernate.validator.constraints.NotBlank;");
-					}
-				} else if (DataType.Date.equals(attr.getDataType()) || DataType.Time.equals(attr.getDataType())
-						|| DataType.Timestamp.equals(attr.getDataType())) {
-					imports.add("import java.util.Date;");
-					imports.add("import javax.persistence.Temporal;");
-					imports.add("import javax.persistence.TemporalType;");
-				} else if (DataType.Enum.equals(attr.getDataType())) {
-					imports.add("import javax.persistence.EnumType;");
-					imports.add("import javax.persistence.Enumerated;");
-				} else if (DataType.BigDecimal.equals(attr.getDataType())) {
-					imports.add("import java.math.BigDecimal;");
-				}
-				if (attr.getPattern() != null) {
-					imports.add("import javax.validation.constraints.Pattern;");
-				}
+    /**
+     * Imports for Java-Entities Bean Validation and DataTypes for EntityGeneration.
+     *
+     * @param domain - definition of domain.
+     * @return Set of Java imports
+     */
+    public static Set<String> gatherEntityImports(Domain domain) {
+        Set<String> imports = new HashSet<String>();
+        if (domain.getAttributes() != null) {
+            for (Attribute attr : domain.getAttributes()) {
 
-				if (!DataType.String.equals(attr.getDataType())) {
-					if (!attr.isNullable()) {
-						imports.add("import javax.validation.constraints.NotNull;");
-					}
-					if (attr.getMax() != null) {
-						imports.add("import javax.validation.constraints.Max;");
-					}
-					if (attr.getMin() != null) {
-						imports.add("import javax.validation.constraints.Min;");
-					}
-				}
-			}
-		}
-		return imports;
-	}
+                if (DataType.String.equals(attr.getDataType())) {
+                    if (attr.getMax() != null || attr.getMin() != null) {
+                        imports.add("import javax.validation.constraints.Size;");
+                    }
+                    if (!attr.isNullable()) {
+                        imports.add("import javax.validation.constraints.NotBlank;");
+                    }
+                } else if (DataType.Date.equals(attr.getDataType()) || DataType.Time.equals(attr.getDataType())
+                        || DataType.Timestamp.equals(attr.getDataType())) {
+                    imports.add("import java.util.Date;");
+                    imports.add("import javax.persistence.Temporal;");
+                    imports.add("import javax.persistence.TemporalType;");
+                } else if (DataType.Enum.equals(attr.getDataType())) {
+                    imports.add("import javax.persistence.EnumType;");
+                    imports.add("import javax.persistence.Enumerated;");
+                } else if (DataType.BigDecimal.equals(attr.getDataType())) {
+                    imports.add("import java.math.BigDecimal;");
+                }
+                if (attr.getPattern() != null) {
+                    imports.add("import javax.validation.constraints.Pattern;");
+                }
+
+                if (!DataType.String.equals(attr.getDataType())) {
+                    if (!attr.isNullable()) {
+                        imports.add("import javax.validation.constraints.NotNull;");
+                    }
+                    if (attr.getMax() != null) {
+                        imports.add("import javax.validation.constraints.Max;");
+                    }
+                    if (attr.getMin() != null) {
+                        imports.add("import javax.validation.constraints.Min;");
+                    }
+                }
+            }
+        }
+        return imports;
+    }
 }
