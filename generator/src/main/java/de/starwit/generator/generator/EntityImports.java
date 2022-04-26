540ea3e3f057e2f23fc4a4c29aeabc30db7b0674
Anett Hübner <anett_huebner@gmx.de>
2022-04-27 00:56:24 +0200

    extractMethods

--- generator/src/main/java/de/starwit/generator/generator/EntityImports.java
+++ generator/src/main/java/de/starwit/generator/generator/EntityImports.java
@@ -22,67 +22,74 @@ public class EntityImports {
      * @return Set of Java imports
      */
     public static Set<String> gatherEntityImports(Domain domain) {
-        Set<String> imports = new HashSet<String>();
+        Set<String> imports = new HashSet<>();
         if (domain.getAttributes() != null) {
             for (Attribute attr : domain.getAttributes()) {
-
-                if (DataType.String.equals(attr.getDataType())) {
-                    if (attr.getMax() != null || attr.getMin() != null) {
-                        imports.add("import javax.validation.constraints.Size;");
-                    }
-                    if (!attr.isNullable()) {
-                        imports.add("import javax.validation.constraints.NotBlank;");
-                    }
-                } else if (DataType.Date.equals(attr.getDataType()) || DataType.Time.equals(attr.getDataType())
-                        || DataType.Timestamp.equals(attr.getDataType())) {
-                    imports.add("import java.util.Date;");
-                    imports.add("import javax.persistence.Temporal;");
-                    imports.add("import javax.persistence.TemporalType;");
-                } else if (DataType.Enum.equals(attr.getDataType())) {
-                    imports.add("import javax.persistence.EnumType;");
-                    imports.add("import javax.persistence.Enumerated;");
-                } else if (DataType.BigDecimal.equals(attr.getDataType())) {
-                    imports.add("import java.math.BigDecimal;");
-                }
-                if (attr.getPattern() != null) {
-                    imports.add("import javax.validation.constraints.Pattern;");
-                }
-
-                if (!DataType.String.equals(attr.getDataType())) {
-                    if (!attr.isNullable()) {
-                        imports.add("import javax.validation.constraints.NotNull;");
-                    }
-                    if (attr.getMax() != null) {
-                        imports.add("import javax.validation.constraints.Max;");
-                    }
-                    if (attr.getMin() != null) {
-                        imports.add("import javax.validation.constraints.Min;");
-                    }
-                }
+                addImportsForAttribute(imports, attr);
             }
         }
 
         if (domain.getRelationships() != null) {
             for (Relationship relation : domain.getRelationships()) {
-                imports.add("import javax.persistence." + relation.getRelationshipType() + ";");
-                if (RelationshipType.ManyToMany.equals(relation.getRelationshipType()) ||
-                        RelationshipType.ManyToOne.equals(relation.getRelationshipType())) {
-                    imports.add("import javax.persistence.JoinColumn;");
-                }
-                if (RelationshipType.ManyToMany.equals(relation.getRelationshipType()) &&
-                        relation.isOwnerSide()) {
-                    imports.add("import javax.persistence.JoinTable;");
-                }
-                if (RelationshipType.OneToMany.equals(relation.getRelationshipType())
-                        || RelationshipType.ManyToMany.equals(relation.getRelationshipType())) {
-                    imports.add("import java.util.Set;");
-                }
-                if (RelationshipType.OneToOne.equals(relation.getRelationshipType()) &&
-                        relation.isOwnerSide()) {
-                    imports.add("import javax.persistence.CascadeType;");
-                }
+                addImportsForRelations(imports, relation);
             }
         }
         return imports;
+    }
+
+    private static void addImportsForAttribute(Set<String> imports, Attribute attr) {
+        if (DataType.String.equals(attr.getDataType())) {
+            if (attr.getMax() != null || attr.getMin() != null) {
+                imports.add("import javax.validation.constraints.Size;");
+            }
+            if (!attr.isNullable()) {
+                imports.add("import javax.validation.constraints.NotBlank;");
+            }
+        } else if (DataType.Date.equals(attr.getDataType()) || DataType.Time.equals(attr.getDataType())
+                || DataType.Timestamp.equals(attr.getDataType())) {
+            imports.add("import java.util.Date;");
+            imports.add("import javax.persistence.Temporal;");
+            imports.add("import javax.persistence.TemporalType;");
+        } else if (DataType.Enum.equals(attr.getDataType())) {
+            imports.add("import javax.persistence.EnumType;");
+            imports.add("import javax.persistence.Enumerated;");
+        } else if (DataType.BigDecimal.equals(attr.getDataType())) {
+            imports.add("import java.math.BigDecimal;");
+        }
+        if (attr.getPattern() != null) {
+            imports.add("import javax.validation.constraints.Pattern;");
+        }
+
+        if (!DataType.String.equals(attr.getDataType())) {
+            if (!attr.isNullable()) {
+                imports.add("import javax.validation.constraints.NotNull;");
+            }
+            if (attr.getMax() != null) {
+                imports.add("import javax.validation.constraints.Max;");
+            }
+            if (attr.getMin() != null) {
+                imports.add("import javax.validation.constraints.Min;");
+            }
+        }
+    }
+
+    private static void addImportsForRelations(Set<String> imports, Relationship relation) {
+        imports.add("import javax.persistence." + relation.getRelationshipType() + ";");
+        if (RelationshipType.ManyToMany.equals(relation.getRelationshipType()) ||
+                RelationshipType.ManyToOne.equals(relation.getRelationshipType())) {
+            imports.add("import javax.persistence.JoinColumn;");
+        }
+        if (RelationshipType.ManyToMany.equals(relation.getRelationshipType()) &&
+                relation.isOwnerSide()) {
+            imports.add("import javax.persistence.JoinTable;");
+        }
+        if (RelationshipType.OneToMany.equals(relation.getRelationshipType())
+                || RelationshipType.ManyToMany.equals(relation.getRelationshipType())) {
+            imports.add("import java.util.Set;");
+        }
+        if (RelationshipType.OneToOne.equals(relation.getRelationshipType()) &&
+                relation.isOwnerSide()) {
+            imports.add("import javax.persistence.CascadeType;");
+        }
     }
 }
