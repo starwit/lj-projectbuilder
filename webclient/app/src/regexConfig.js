3ce34fbc7db4ae61f1680fd3c4a12cb6af1c5842
Anett Hübner <anett_huebner@gmx.de>
2022-04-26 08:50:35 +0200

    feat: allow # in branchname

--- webclient/app/src/regexConfig.js
+++ webclient/app/src/regexConfig.js
@@ -6,8 +6,8 @@ const RegexConfig = {
     packageName: /^[A-Za-z0-9]{1,100}$/,
 
     appTemplateLocation: /^(?:git|ssh|https?|git@[-w.]+):(\/\/)?([a-zA-Z0-9./_-]*)(.git)$/,
-    appTemplateBranch: /^([a-zA-Z0-9/_-]{0,100})$/,
+    appTemplateBranch: /^([a-zA-Z0-9#/_\\-]{0,100})$/,
 
-    relationship: /^(|([a-z][a-zA-Z0-9]{0,100}))$/
+    relationship: /^([a-z][a-zA-Z0-9]{0,100})$/
 };
 export default RegexConfig;
