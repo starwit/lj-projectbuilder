fe9eab5eeec87667de567bb5d41d9b0861ea6f1b
Cedric Rische <cedricrische@gmail.com>
2022-05-20 23:12:06 +0200

    feat: AB#248 Create standardized layout Look&Theme Cleanup

--- webclient/app/src/app/App.jsx
+++ webclient/app/src/app/App.jsx
@@ -2,12 +2,10 @@ import React from "react";
 import AppHeader from "./commons/appHeader/AppHeader";
 import MainContentRouter from "./pages/MainContentRouter";
 import {CssBaseline} from "@mui/material";
-import AppStyles from "./AppStyles";
 import ErrorHandler from "./commons/errorHandler/ErrorHandler";
 import {appMenuItems} from "./AppConfig";
 
 function App() {
-    const appStyles = AppStyles();
 
     return (
         <React.Fragment>
