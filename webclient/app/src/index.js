23b0926087378dd9921df16a8bb4223d4e98d5e6
Cedric Rische <cedricrische@gmail.com>
2022-06-17 09:11:05 +0200

    feat: AB#249 Sentry Integration Added sentry to frontend

--- webclient/app/src/index.js
+++ webclient/app/src/index.js
@@ -7,16 +7,28 @@ import {HashRouter as Router} from "react-router-dom";
 import "./localization/i18n";
 import {SnackbarProvider} from "notistack";
 import MainTheme from "./app/commons/mainTheme/MainTheme";
+import {BrowserTracing} from "@sentry/tracing";
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+    dsn: "[ENTER DSN KEY HERE!]",
+    integrations: [new BrowserTracing()],
+
+    // We recommend adjusting this value in production, or using tracesSampler
+    // for finer control
+    tracesSampleRate: 1.0,
+    enabled: process.env.NODE_ENV !== "development"
+});
 
 ReactDOM.render((
-    <Router>
-        <MainTheme>
-            <SnackbarProvider maxSnack={5}>
-                <App/>
-            </SnackbarProvider>
-        </MainTheme>
-    </Router>
-),
+        <Router>
+            <MainTheme>
+                <SnackbarProvider maxSnack={5}>
+                    <App/>
+                </SnackbarProvider>
+            </MainTheme>
+        </Router>
+    ),
 document.getElementById("root")
 )
 ;
