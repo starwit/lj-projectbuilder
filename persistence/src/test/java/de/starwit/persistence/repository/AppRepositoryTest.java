57d58b3770ead2da75fceb69eb52762bac8a3920
Anett Hübner <anett_huebner@gmx.de>
2022-06-21 21:00:17 +0200

    bugfix database

--- persistence/src/test/java/de/starwit/persistence/repository/AppRepositoryTest.java
+++ persistence/src/test/java/de/starwit/persistence/repository/AppRepositoryTest.java
@@ -8,13 +8,16 @@ import java.util.List;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 
+import de.starwit.persistence.RepositoryTest;
 import de.starwit.persistence.entity.App;
 import de.starwit.persistence.entity.AppTemplate;
 
-@DataJpaTest
+@RepositoryTest
 public class AppRepositoryTest {
 
     @Autowired
