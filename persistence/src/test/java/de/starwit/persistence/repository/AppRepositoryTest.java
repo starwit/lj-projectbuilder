c19bd00c5d171c7202c2d8d45704fb03a2d331c5
Anett Hübner <anett_huebner@gmx.de>
2022-06-21 21:10:39 +0200

    undo changes of test

--- persistence/src/test/java/de/starwit/persistence/repository/AppRepositoryTest.java
+++ persistence/src/test/java/de/starwit/persistence/repository/AppRepositoryTest.java
@@ -8,16 +8,13 @@ import java.util.List;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 
-import de.starwit.persistence.RepositoryTest;
 import de.starwit.persistence.entity.App;
 import de.starwit.persistence.entity.AppTemplate;
 
-@RepositoryTest
+@DataJpaTest
 public class AppRepositoryTest {
 
     @Autowired
