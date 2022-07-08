9676979987f1186079bd1d9d1faa8f73dcb7dfd8
Cedric Rische <cedric.rische@cedsoft.de>
2022-07-08 11:41:28 +0200

    Removed docker env replacement

--- webclient/app/src/app/AppConfig.js
+++ webclient/app/src/app/AppConfig.js
@@ -4,9 +4,9 @@ const appMenuItems = [
 ];
 
 const sentryConfiguration = {
-    dsn: process.env.SENTRY_DSN_FRONTEND,
-    tracesSampleRate: Number(process.env.SENTRY_TRACES_SAMPLE_RATE_FRONTEND || .75),
-    environment: process.env.SENTRY_ENVIRONMENT
+    dsn: "https://89ae8cc61d2545a2a88021f96a433b43@o1305630.ingest.sentry.io/6548197",
+    tracesSampleRate: .75
+    // environment: process.env.SENTRY_ENVIRONMENT
 };
 
 export {appMenuItems, sentryConfiguration};
