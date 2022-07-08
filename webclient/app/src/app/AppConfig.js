872edb1f2e6c104f44a151c6e33653675bf44c73
Cedric Rische <cedric.rische@cedsoft.de>
2022-07-08 11:56:40 +0200

    Added config controller and configurationRest configured frontend to use sentry

--- webclient/app/src/app/AppConfig.js
+++ webclient/app/src/app/AppConfig.js
@@ -4,9 +4,7 @@ const appMenuItems = [
 ];
 
 const sentryConfiguration = {
-    dsn: "https://89ae8cc61d2545a2a88021f96a433b43@o1305630.ingest.sentry.io/6548197",
     tracesSampleRate: .75
-    // environment: process.env.SENTRY_ENVIRONMENT
 };
 
 export {appMenuItems, sentryConfiguration};
--- webclient/app/src/app/commons/sentryInit/SentryInit.jsx
+++ webclient/app/src/app/commons/sentryInit/SentryInit.jsx
@@ -1,19 +1,25 @@
-import React from "react";
+import React, {useEffect, useMemo} from "react";
 import * as Sentry from "@sentry/react";
 import {BrowserTracing} from "@sentry/tracing";
 import {sentryConfiguration} from "../../AppConfig";
+import ConfigurationRest from "../../services/ConfigurationRest";
 
 function SentryInit(props) {
-    Sentry.init({
-        dsn: sentryConfiguration.dsn,
-        integrations: [new BrowserTracing()],
+    const configurationRest = useMemo(() => new ConfigurationRest(), []);
 
-        // We recommend adjusting this value in production, or using tracesSampler
-        // for finer control
-        tracesSampleRate: sentryConfiguration.tracesSampleRate,
-        enabled: process.env.NODE_ENV !== "development",
-        environment: sentryConfiguration.environment
-    });
+    useEffect(() => {
+        configurationRest.getSentryDsn().then(response => {
+            Sentry.init({
+                dsn: response.data,
+                integrations: [new BrowserTracing()],
+
+                // We recommend adjusting this value in production, or using tracesSampler
+                // for finer control
+                tracesSampleRate: sentryConfiguration.tracesSampleRate,
+                enabled: process.env.NODE_ENV !== "development"
+            });
+        });
+    }, [configurationRest]);
 
     return (
         <></>
